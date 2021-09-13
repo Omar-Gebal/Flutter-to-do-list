35f4f874dc25286fb1da8beca725bfb2820eb698
Omar Emad <2001omaremad@gmail.com>
2021-09-13 17:59:08 +0200

    initialised app

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('To do list'),
+        ),
+        body: Center(
+          child: Text('initialization'),
+        ),
+      ),
+    );
+  }
+}
