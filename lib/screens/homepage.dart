bbef66183b4e5e4aef8e920117abd7968d997510
Omar Emad <2001omaremad@gmail.com>
2021-09-17 16:48:53 +0200

    Homepage ui done (placeholder)

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todolist/widgets.dart';
 
 class Home extends StatefulWidget {
   const Home({Key key}) : super(key: key);
@@ -13,12 +14,34 @@ class _HomeState extends State<Home> {
     return Scaffold(
       body: SafeArea(
         child: Container(
+          width: double.infinity,
           padding: EdgeInsets.symmetric(horizontal: 20, vertical: 25),
-          child: Column(
+          child: Stack(
+            alignment: Alignment.center,
             children: [
-              Image.asset(
-                'images/logo.png',
-                height: 40,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(bottom: 20),
+                    child: Image.asset(
+                      'images/logo.png',
+                      height: 40,
+                    ),
+                  ),
+                  TaskCard(),
+                ],
+              ),
+              Positioned(
+                bottom: 0,
+                child: Container(
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.pink,
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  child: Text('Add a new task'),
+                ),
               ),
             ],
           ),
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
 
+class TaskCard extends StatelessWidget {
+  final title;
+  final description;
+  TaskCard({this.title, this.description});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom: 20),
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 25),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15.0), color: Colors.blue),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title ?? "unnamed task",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          Text(description ?? 'That is a task without a description')
+        ],
+      ),
+    );
+  }
+}
