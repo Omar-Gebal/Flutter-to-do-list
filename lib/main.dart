8c1c6fc788741faf82e113302e73109acd8f877f
Omar Emad <2001omaremad@gmail.com>
2021-09-17 15:43:39 +0200

    added temp images

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,28 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var previewText = 'not changed';
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text('To do list'),
         ),
         body: Center(
-          child: Text('initialization'),
+          child: Column(
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter a new todo item'),
+                onChanged: (input) {
+                  previewText = input;
+                },
+              ),
+              Text('$previewText'),
+              Image(image: AssetImage('images/back_arrow_icon.png'))
+            ],
+          ),
         ),
       ),
     );
